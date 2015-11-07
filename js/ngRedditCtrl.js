f233950745afcf00a92e12e8e6a9034e655f6c2f
josh-stevens <jcstevens12@outlook.com>
2015-11-07 12:20:33 -0600

    Loaded Reddit JSON

--- js/ngRedditCtrl.js
+++ js/ngRedditCtrl.js
@@ -1,3 +1,23 @@
-ngReddit.controller('NgRedditCtrl', function($scope, $http) {
+ngReddit.controller('NgRedditCtrl', function($scope, Reddit) {
+
+  // Default to /r/funny
+  $scope.category = "funny";
+
+  // Function is called on page load (line 15) and every time the
+  // search field is changed
+  $scope.getPosts = function() {
+    Reddit.getPosts($scope.category)
+      .then(function(res) {
+        $scope.posts = res.data.data.children;
+        $scope.posts.map(function(post) {
+          if (post.data.thumbnail === "" || post.data.thumbnail === "nsfw") {
+            post.data.thumbnail = "images/unknown.png";
+          }
+        });
+        console.log("Posts: ", $scope.posts);
+      });
+  };
+
+  $scope.getPosts();
 
 });
